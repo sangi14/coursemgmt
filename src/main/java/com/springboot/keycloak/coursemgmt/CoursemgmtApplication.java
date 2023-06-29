b09d536c3d2fa7fbe0bbf46a24248fc515ba2fc9
sangi14 <sangishivangi14@gmail.com>
2023-06-29 16:34:03 +0530

    first commit

---
+++ src/main/java/com/springboot/keycloak/coursemgmt/CoursemgmtApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.keycloak.coursemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/keycloak/coursemgmt/CoursemgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.keycloak.coursemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
